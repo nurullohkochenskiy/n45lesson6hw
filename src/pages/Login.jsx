ad892ddfba807394ab44edcc1eafefa098777960
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-04-07 18:15:02 +0500

    formstate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,34 +3,49 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
   const form = useForm();
-  const { register } = form;
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+  console.log(errors);
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <div className="login-container">
       <div className="login-form">
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <div className="form-group">
             <label htmlFor="username">Username:</label>
             <input
               type="text"
               id="username"
-              {...register("username")}
+              {...register("username", {
+                required: {
+                  value: true,
+                  message: "username is required",
+                },
+              })}
               // value={values.username}
-              // className={errors.username ? 'error' : ''}
+              className={errors.username ? 'error' : ''}
             />
-            {/* {errors.username && <span className="error-text">{errors.username}</span>} */}
+            {errors.username && <span className="error-text">{errors.username.message}</span>}
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
             <input
               type="password"
               id="password"
-              {...register("password")}
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "password is required",
+                },
+              })}
               // value={values.password}
               // onChange={handleChange}
-              // className={errors.password ? 'error' : ''}
+              className={errors.password ? 'error' : ''}
             />
-            {/* {errors.password && <span className="error-text">{errors.password}</span>} */}
+            {errors.password && <span className="error-text">{errors.password.message}</span>}
           </div>
           <button type="submit" className="submit-button">
             Login
